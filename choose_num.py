4d7b13c41eff45f466175cf6e428437f8f99daf7
peonies7 <cassandramjoseph@gmail.com>
2026-02-16 19:39:40 -0500

    began creating function to guess number

--- choose_num.py
+++ choose_num.py
@@ -1,2 +1,21 @@
+import numpy as np
+
+
 """ generates the random number """
+
+#creates random number using numpy random module
+random_num = np.random.randint(1,101)
+print(random_num)
+
+user_guess = int(input("Enter your guess: "))
+
+if user_guess == random_num:
+    print("On point!")
+elif user_guess > random_num:
+    print("Too big!")
+elif user_guess < random_num:
+    print("Too small!")
+    
+
+
 
